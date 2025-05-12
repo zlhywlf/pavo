8c9d64273a474bfa219f35e246ae0964b901597c
zlhywlf <tommietanghao@zlhywlf.onmicrosoft.com>
2025-05-12 10:55:11 +0800

    note

--- buildSrc/src/test/kotlin/zlhywlf/utils/PlatformTest.kt
+++ buildSrc/src/test/kotlin/zlhywlf/utils/PlatformTest.kt
@@ -1,12 +1,40 @@
 package zlhywlf.utils
 
-import org.junit.jupiter.api.Test
+import com.github.javafaker.Faker
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
 class PlatformTest {
 
-    @Test
-    internal fun parseOsTypeTest() {
-        assert(parseOsType("windows") == OsType.WINDOWS)
+    @ParameterizedTest
+    @MethodSource("platformData")
+    internal fun parseOsTypeTest(key: String, ex: OsType) {
+        val len = 5
+        val pre = faker.lorem().fixedString(len)
+        val post = faker.lorem().fixedString(len)
+        assert(parseOsType("$pre$key$post") == ex)
+    }
+
+    companion object {
+        lateinit var faker: Faker
+
+        @BeforeAll
+        @JvmStatic
+        fun setupFaker() {
+            faker = Faker()
+        }
+
+        @JvmStatic
+        fun platformData(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of("windows", OsType.WINDOWS),
+                Arguments.of("mac", OsType.MAC),
+                Arguments.of("linux", OsType.LINUX),
+            )
+        }
     }
 
 }
