f9048eb5522fd040a867e82fbcc575ab1aadeed8
zlhywlf <tommietanghao@zlhywlf.onmicrosoft.com>
2025-06-09 10:26:31 +0800

    note

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,9 +3,9 @@ plugins {
 }
 
 dependencies {
-    testImplementation("com.github.javafaker:javafaker:1.0.2")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.12.2")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation(libs.javafaker)
+    testImplementation(libs.junit.jupiter)
+    testRuntimeOnly(libs.junit.jupiter.launcher)
 }
 
 tasks.named<Test>("test") {
